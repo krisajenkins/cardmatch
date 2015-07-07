ad1113a00f5e27065d5393d4e81d7f81f0459c44
Kris Jenkins <krisajenkins@gmail.com>
2015-07-07 23:25:55 +0100

    Correcting a path.

--- elm.js
+++ elm.js
@@ -58,7 +58,7 @@ Elm.API.make = function (_elm) {
    var decodeProducts = $Json$Decode.list(decodeProduct);
    var products = A2($Signal.map,
    $Http.mapResult($Json$Decode.decodeString(decodeProducts)),
-   $Http.sendGet($Signal.constant("/products.json")));
+   $Http.sendGet($Signal.constant("products.json")));
    _elm.API.values = {_op: _op
                      ,products: products};
    return _elm.API.values;
